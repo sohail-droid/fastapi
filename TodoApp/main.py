4fdb90d208d6ad47a211d7cd16f55f5043942d03
sohail-droid <sohailsmd3131@gmail.com>
2026-06-21 13:01:56 +0530

    fastapi with sqlalchemy

---
+++ TodoApp/main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+import models
+from database import engine
+
+
+app = FastAPI()
+
+#this line creates the tables in the database based on the models defined in models.py. 
+# It uses the metadata from the Base class to create all tables that are defined as subclasses of Base.
+models.Base.metadata.create_all(bind=engine)
